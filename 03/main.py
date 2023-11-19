a9fd77f0945943f161deca1784da564fcfc622d0
matinraisi <raisi.m630@gmail.com>
2023-11-19 10:32:55 +0330

    learning git jalase4

---
+++ 03/main.py
@@ -0,0 +1,60 @@
+str_ = 'asnbl?'
+# int_ = 10
+# list_ = [5,5421,465,21,86,821,'asjckl;',True,[]]
+
+# dict_ = {
+#     'name':'matin',
+#     'age' :'22',
+#     'phone': +989156308230,
+#     'active':False,
+#     'cars':['praide','nisan']
+# }
+
+# print(dict_.get('matin'))
+# print(dict_.items())
+# print(dict_.keys())
+# print(dict_.values())
+
+# num1= float(user)
+# if num1 == int:
+#     # print(num1//1)
+#     print(num1)
+
+# user = int(input("enter your : "))
+
+
+# if user % 2 == 0:
+#     print(f"in add zoje {user}")
+# else:
+#     print(f"in add fard {user}")
+
+
+
+# user = input("enter your : ")
+# list_  = user.split(',')
+# zoje = []
+# for i in list_:
+#     if int(i) % 2 ==0:
+#         zoje.append(i)
+# print(zoje)
+
+
+# star = '*'
+# add = 0
+# while True:
+#     add+=1
+#     if add < 6:
+#         print(star *add)
+#     # elif add > 6:
+#     else:    
+#         print(star)
+#         break
+# *
+# **
+# ***
+# ****
+# *****
+# ****
+# ***
+# **
+# *
