6bd51824ad7595d1bd516d179b93f03917915955
matinraisi <raisi.m630@gmail.com>
2023-11-21 13:38:29 +0330

    upload lessn 05

---
+++ 05/main.py
@@ -0,0 +1,57 @@
+numbers_list = [1,2,4,5,5,8,5,8,9,5,16,7468,354]
+
+# numbers.pop()
+
+# numbers.append(10)
+# # numbers.index()
+# print(numbers)
+# numbers.sort() 
+
+# x=10
+# f(x)=x+5
+
+# def sayhello():
+#     # print("hello python")
+#     zarb = 2*2
+#     print(zarb)
+
+# # print(sayhello())
+
+# fulname = sayhello()
+
+# print(2 + fulname)
+
+
+# def Sum():
+#     numbers = 5 + 6
+#     # print(numbers)
+#     return numbers
+
+# # Sum()
+# # print(Sum())
+# tmp = Sum()
+# plus = tmp + 10
+# print(plus)
+
+
+# def number(num1 ):
+#     numbers  = num1 + 10
+#     return numbers
+
+# print(number(10))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
